bd0c99f190048169828a5081d62312822e758d88
Ste-Viera <stephanie_holzschuk@hotmail.com>
2022-08-09 21:48:03 -0300

    crud

---
+++ js/exercicio4.js
@@ -0,0 +1,37 @@
+$("body").attr("class", "container mt-4");
+
+$("<main>", { class: "row" }).append(
+  $("<section>", { class: "col" }).append(
+    $("<div>", { class: "card" }).append(
+      $("div", { class: "card-header", text: "" }),
+      $("<div>", { class: "card-body" }).append(
+        $("<table>", { class: "table table-bordered text-center" }).append(
+          $("<thead>").append(
+            $("<th>", { text: "Nome" }),
+            $("<th>", { text: "Idade" }),
+            $("<th>", { text: "Cidade" }),
+          ),
+          $("<tbody>").append(
+            $("<tr>").append(
+              $("<td>", { text: "Stephanie" }),
+              $("<td>", { text: "28" }),
+              $("<td>", { text: "Palhoça" })
+            ),
+            $("<tr>").append(
+              $("<td>", { text: "Henrique" }),
+              $("<td>", { text: "19" }),
+              $("<td>", { text: "Florianópolis" })
+            ),
+            $("<tr>").append(
+              $("<td>", { text: "Henrique" }),
+              $("<td>", { text: "19" }),
+              $("<td>", { text: "Florianópolis" })
+            )
+          )
+        )
+      ),
+      $("<div>", { class: "card-footer", text: "Página desenvolvida como Exercício" })
+    )
+  )
+).appendTo("body");
+
